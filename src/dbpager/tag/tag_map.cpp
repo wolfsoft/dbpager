8e2af55960ba30f2e972b8dfd177c73aeaded47b
Dennis Prochko <wolfsoft@mail.ru>
2017-12-30 23:43:50 +0300

    dbp::map json import/export

--- src/dbpager/tag/tag_map.cpp
+++ src/dbpager/tag/tag_map.cpp
@@ -20,6 +20,7 @@
  */
 
 #include <dcl/strutils.h>
+#include <json/json.h>
 
 #include "tag/tag_map.h"
 
@@ -132,6 +133,25 @@ void tag_map_from_json::execute(context &ctx, std::ostream &out, const tag *call
 		  (format(_("map '{0}' was not defined in the current context")) % name).str());
 	}
 	string_map *data = (string_map*)from_string<void*>(data_ptr);
+
+	ostringstream s(ostringstream::out | ostringstream::binary);
+	tag_impl::execute(ctx, s, caller);
+
+	Json::Value root;
+	Json::Reader reader;
+	if (!reader.parse(s.str(), root)) {
+		throw dbp::exception(_("invalid JSON structure of the request"));
+	}
+
+	if (root.isObject()) {
+		data->clear();
+		// parse json object members
+		Json::Value::Members members = root.getMemberNames();
+		for (Json::Value::Members::const_iterator i = members.begin(); i != members.end(); ++i) {
+			if (root[*i].isConvertibleTo(Json::ValueType::stringValue))
+				data->insert({*i, root[*i].asString()});
+		}
+	}
 }
 
 void tag_map_to_json::execute(context &ctx, std::ostream &out, const tag *caller) const {
@@ -143,6 +163,14 @@ void tag_map_to_json::execute(context &ctx, std::ostream &out, const tag *caller
 		  (format(_("map '{0}' was not defined in the current context")) % name).str());
 	}
 	string_map *data = (string_map*)from_string<void*>(data_ptr);
+
+	Json::Value root;
+	for (auto it = data->begin(); it != data->end(); ++it) {
+		root[it->first] = it->second;
+	}
+
+	Json::FastWriter writer;
+	out << writer.write(root);
 }
 
 } // namespace
